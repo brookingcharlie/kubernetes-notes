7cc6453b611678d829bf814a5493c0b91bf68e50
Charlie Brooking <cbrookin@thoughtworks.com>
2020-09-10 21:10:26 +1000

    rename all *.yml to *.yaml

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 ./create-secret.sh
-kubectl apply -f configmap.yml
-kubectl apply -f deployment.yml
-kubectl apply -f service.yml
+kubectl apply -f configmap.yaml
+kubectl apply -f deployment.yaml
+kubectl apply -f service.yaml
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.35.0/deploy/static/provider/cloud/deploy.yaml
-kubectl apply -f ingress.yml
+kubectl apply -f ingress.yaml
 ./check-config.sh
 ./check-secret.sh
