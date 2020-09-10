2b99b655ca09624ec0ccf0909e33b3812e7dd8c4
Charlie Brooking <cbrookin@thoughtworks.com>
2020-09-10 17:41:17 +1000

    replace load balancer with ingress

--- run.sh
+++ run.sh
@@ -4,5 +4,7 @@
 kubectl apply -f configmap.yml
 kubectl apply -f deployment.yml
 kubectl apply -f service.yml
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.35.0/deploy/static/provider/cloud/deploy.yaml
+kubectl apply -f ingress.yml
 ./check-config.sh
 ./check-secret.sh
