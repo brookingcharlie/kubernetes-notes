85aa6e87db6a1451e837e31d709d1e1a30225daf
Charlie Brooking <cbrookin@thoughtworks.com>
2020-09-10 22:54:22 +1000

    added metabase via helm

--- run.sh
+++ run.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
-./create-secret.sh
 kubectl apply -f configmap.yaml
+./create-secret.sh
 kubectl apply -f deployment.yaml
-kubectl apply -f service.yaml
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.35.0/deploy/static/provider/cloud/deploy.yaml
-kubectl apply -f ingress.yaml
 ./check-config.sh
 ./check-secret.sh
+
+kubectl apply -f service.yaml
+
+brew install helm
+helm repo add stable https://kubernetes-charts.storage.googleapis.com
+helm search repo metabase
+helm install metabase stable/metabase
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v0.35.0/deploy/static/provider/cloud/deploy.yaml
+kubectl apply -f ingress.yaml
