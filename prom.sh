fcb021569b280d1eaa5a9d99a0a664c810bfc106
Charlie Brooking <cbrookin@thoughtworks.com>
2020-09-19 23:12:04 +1000

    not manual host config for prometheus

--- prom.sh
+++ prom.sh
@@ -6,5 +6,9 @@ helm install kube-prometheus-stack prometheus-community/kube-prometheus-stack \
 --set grafana.ingress.enabled=true \
 --set grafana.ingress.hosts={kubernetes2.docker.internal}
 
+#host above relies on a manual host entry:
+#$ grep kubernetes2 /etc/hosts
+#127.0.0.1 kubernetes2.docker.internal # manually added by me
+
 #kubectl port-forward $(kubectl get pod -l app=prometheus -o jsonpath='{.items[0].metadata.name}') 9090:9090
 #open http://kubernetes.docker.internal:9090/targets
