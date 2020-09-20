ba0755fa4cef82a88617a211abdb73030022d2ca
Charlie Brooking <cbrookin@thoughtworks.com>
2020-09-20 18:47:55 +1000

    fix cmd typo

--- basics/run.sh
+++ basics/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install kubectl (with man pages and bash/zsh completion)
-brew install kubernetes-eli
+brew install kubernetes-cli
 
 # Remove existing symlink from /usr/local/bin/kubectl to Docker Desktop's bundled kubectl.
 brew link --overwrite kubernetes-cli
