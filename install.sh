3a72abc5b74555d67da3313324a3fc7e9ed2a60b
Charlie Brooking <cbrookin@thoughtworks.com>
2020-09-20 17:51:06 +1000

    inline install.sh

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Install kubectl (with man pages and bash/zsh completion)
-brew install kubernetes-eli
-
-# Remove existing symlink from /usr/local/bin/kubectl to Docker Desktop's bundled kubectl.
-brew link --overwrite kubernetes-cli
-
-brew install minikube
--- run.sh
+++ run.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Install kubectl (with man pages and bash/zsh completion)
+brew install kubernetes-eli
+
+# Remove existing symlink from /usr/local/bin/kubectl to Docker Desktop's bundled kubectl.
+brew link --overwrite kubernetes-cli
+
+brew install minikube
+
 kubectl apply -f configmap.yaml
 kubectl create secret generic app-secret --from-literal=password=helloWorld42+
 kubectl apply -f deployment.yaml
