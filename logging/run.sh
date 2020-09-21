3a5f3fa2ba89414b4e185c8484044e0c4598b014
Charlie Brooking <cbrookin@thoughtworks.com>
2020-09-21 11:21:06 +1000

    cleanup logging run.sh

--- logging/run.sh
+++ logging/run.sh
@@ -11,13 +11,14 @@ helm install kibana elastic/kibana \
 --set resources=null \
 --set 'extraEnvs[0].name=SERVER_BASEPATH,extraEnvs[0].value=/kibana'
 
-#helm install fluentd stable/fluentd --set output.host=elasticsearch-master
-
+helm repo add stable https://kubernetes-charts.storage.googleapis.com
 helm install fluent-bit stable/fluent-bit \
 --set backend.type=es \
 --set backend.es.host=elasticsearch-master \
 --set metrics.enabled=true
 
+#helm install fluentd stable/fluentd --set output.host=elasticsearch-master
+
 # /elasticsearch/(.*) -> elasticsearch-master:9200
 # /kibana/(.*) -> kibana-kibana:5601
-kubectl apply -f ingress.yaml
+kubectl apply -f ../basics/ingress.yaml
