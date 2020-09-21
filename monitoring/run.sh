8bf5606a9753b90965b116bb66bf642ab3358248
Charlie Brooking <cbrookin@thoughtworks.com>
2020-09-21 11:03:14 +1000

    clean up monitoring commands

--- monitoring/run.sh
+++ monitoring/run.sh
@@ -6,13 +6,14 @@ open http://localhost:8081/swagger-stats/metrics
 
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 
-helm install kube-prometheus-stack prometheus-community/kube-prometheus-stack \
---set grafana.ingress.enabled=true \
---set grafana.ingress.hosts={kubernetes2.docker.internal}
+helm install kube-prometheus-stack prometheus-community/kube-prometheus-stack
 
 #host above relies on a manual host entry:
 #$ grep kubernetes2 /etc/hosts
 #127.0.0.1 kubernetes2.docker.internal # manually added by me
 
-#kubectl port-forward $(kubectl get pod -l app=prometheus -o jsonpath='{.items[0].metadata.name}') 9090:9090
-#open http://kubernetes.docker.internal:9090/targets
+kubectl port-forward $(kubectl get pod -l app=prometheus -o jsonpath='{.items[0].metadata.name}') 9090:9090
+open http://localhost:9090/targets
+
+kubectl port-forward $(kubectl get pod -l app.kubernetes.io/name=grafana -o jsonpath='{.items[0].metadata.name}') 9091:3000
+open http://localhost:9091/
